0ccd77acba1e479f31743f6ad1f49c8774260c2d
Sergey Koksharov <gentoosiastic@gmail.com>
2023-07-19 13:48:44 +0300

    feat: describe GraphQL schema for MemberType

---
+++ src/routes/graphql/types/member-types.ts
@@ -0,0 +1,18 @@
+import { GraphQLEnumType, GraphQLObjectType, GraphQLFloat, GraphQLInt } from 'graphql';
+
+const MemberTypeId = new GraphQLEnumType({
+  name: 'MemberTypeId',
+  values: {
+    BASIC: { value: 'basic' },
+    BUSINESS: { value: 'business' },
+  },
+});
+
+export const MemberType = new GraphQLObjectType({
+  name: 'MemberType',
+  fields: () => ({
+    id: { type: MemberTypeId },
+    discount: { type: GraphQLFloat },
+    postsLimitPerMonth: { type: GraphQLInt },
+  }),
+});
