624e6d1065931d971e7fd6f9d5ae2c82d43ebecd
Sergey Koksharov <gentoosiastic@gmail.com>
2023-07-19 14:06:28 +0300

    feat: describe GraphQL schema for Profiles

---
+++ src/routes/graphql/types/profiles.ts
@@ -0,0 +1,11 @@
+import { GraphQLObjectType, GraphQLBoolean, GraphQLInt } from 'graphql';
+import { UUIDType } from './uuid.js';
+
+export const ProfileType = new GraphQLObjectType({
+  name: 'Profile',
+  fields: () => ({
+    id: { type: UUIDType },
+    isMale: { type: GraphQLBoolean },
+    yearOfBirth: { type: GraphQLInt },
+  }),
+});
